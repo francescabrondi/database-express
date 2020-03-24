20e37f1540f14c9ebbd8ec1662a4bc00bcfcb304
Francesca <francescabrondi@gmail.com>
2020-03-24 17:32:58 +0100

    initial

---
+++ routes/products.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("../config/db");
+
+//@route   GET /products
+//@desc    Get all products
+//@access  Public
+//@params  No params
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM products", (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+//@route   GET /:id
+//@desc    Get single products
+//@access  Public
+//@params  id
+router.get("/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM products WHERE id = ?",
+    req.params.id,
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+});
+
+module.exports = router;
